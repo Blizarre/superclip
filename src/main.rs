079ad40500c11685708cfc1184a7b0878a5ac467
Simon M <git@simon.marache.net>
2019-12-12 23:15:12 +0000

    Simple argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate clap;
+use clap::{App, SubCommand};
+
+
 fn main() {
+    let matches = App::new("SuperClip")
+                      .version("0.1")
+                      .author("Simon M. <git@simon.marache.net>")
+                      .about("Your global clipboard")
+                      .subcommand(SubCommand::with_name("copy")
+                                  .about("Copy data into the clipboard"))
+                      .subcommand(SubCommand::with_name("paste")
+                                  .about("Print the clipboard content"))
+                      .get_matches();
+
     println!("Hello, world!");
+
+    if let Some(_matches) = matches.subcommand_matches("copy") {
+        println!("Copy");
+    }
+    if let Some(_matches) = matches.subcommand_matches("paste") {
+        println!("Paste");
+    }
 }
